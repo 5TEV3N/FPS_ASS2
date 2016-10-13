3d4b773729a9b7a400e2722c2a7cf30c3ee8b344
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-13 19:25:43 -0400

    Added PlayerController + InputManager.
    Also set up a scene to test around the controls

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float mouseSensitivity = 1;    //Mouse sensitivity
+    public float playerSpeed = 1;   //We can controll the speed of the player here.
+    public Rigidbody rb;    //Access the rigidbody to move
+    public Camera cam;  //Acess the Camera of the gameobject
+    public float upDownRange = 90.0f;   //How far i can look up or down.
+
+    private float verticalRotation = 0;
+
+    public void Mouselook(float mouseXAxis, float mouseYAxis)
+    {
+        //Filter Horizontal input
+        if (mouseXAxis != 0)
+        {
+            gameObject.transform.Rotate(new Vector3(0, mouseXAxis, 0)); //Rotate gameObject left or right
+            
+        }
+
+        //Filter Vertical input
+        if (mouseYAxis != 0)
+        {
+            verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;    //Rotates the gameobject up and down.
+            verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);    //Clamp the camera from moving up and down. Argument = float Value, float minimum val, float max minimum val
+            cam.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0); //Idk.
+        } 
+    }
+
+    public void PlayerMove(float xAxis, float zAxis)
+    {
+        if (xAxis != 0)
+        {
+            if (xAxis > 0)
+            {
+                //positive x = Right
+                rb.AddForce(transform.right * playerSpeed);
+            }
+
+            if (xAxis < 0)
+            {
+                rb.AddForce(-transform.right * playerSpeed);
+            }
+        }
+
+        if (zAxis != 0)
+        {
+            if (zAxis > 0)
+            {
+                //positive z = Forward
+                rb.AddForce(transform.forward * playerSpeed);
+            }
+
+            if (zAxis < 0)
+            {
+                rb.AddForce(-transform.forward * playerSpeed);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerController playerController;
+    float xAxis = 0; // 1 = right, -1 = left
+    float zAxis = 0; // 1 = front, -1 back
+    float mouseXAxis = 0; // left or right movement of mouse (camera). Positive numb = right, Negative numb = left
+    float mouseYAxis = 0; // up or down movement of mouse (camera). Positive numb = up, Negative numb = down.
+    bool cameraLock = true;
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void FixedUpdate()
+    {
+        mouseXAxis = Input.GetAxis("Mouse X");
+        mouseYAxis = Input.GetAxis("Mouse Y");
+        if (mouseXAxis != 0 || mouseYAxis != 0)
+        {
+            playerController.Mouselook(mouseXAxis, mouseYAxis);
+        }
+
+        xAxis = Input.GetAxisRaw("Horizontal");
+        zAxis = Input.GetAxisRaw("Vertical");
+        if (xAxis != 0 || zAxis != 0)
+        {
+            playerController.PlayerMove(xAxis, zAxis);
+        }
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cameraLock = true;
+
+            if (cameraLock == true )
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                cameraLock = false;
+            }
+        }
+    }
+}
