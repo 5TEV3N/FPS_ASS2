aa61bbdb7c4df54851b2779f331cf92056c9d710
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-25 19:37:59 -0400

    Ammo ui added

--- Assets/Scripts/Managers/AmmoManager.cs
+++ Assets/Scripts/Managers/AmmoManager.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class AmmoManager : MonoBehaviour
 {
-    public int ammo;        // Ammo you start with
-    public int ammoLeft;    // Ammo you have left
-    
+    public int ammo;                // Ammo you start with
+    public int ammoLeft;            // Ammo you have left
+    public Text uiAmmo;             //
+    public Text uiAmmoBackdrop;     //
+
     void Update()
     {
+        uiAmmo.text = ammoLeft + " | " + ammo;
+        uiAmmoBackdrop.text = uiAmmo.text;
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             print("Reloading...");
