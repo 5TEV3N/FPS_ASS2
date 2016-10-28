b5c395fa22adb0f1c9ba3f5f807eb686f5f2a1b6
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-28 17:14:14 -0400

    Edited the requirement.cs

--- Assets/Requirements.cs
+++ Assets/Requirements.cs
@@ -9,25 +9,25 @@ public class NewBehaviourScript : MonoBehaviour
 10 shoot bullet on command  X
 10 can be out of ammo   X
 10 health system (players and enemies) x
-10 enemy shoots at player
+10 enemy shoots at player x
 10 lose by dying    
 10 win by killing all enemies x
 
     EXTRAS (worth 30% total)
 10 enemy pathfinding (to player, ammo...)
-6 different kinds of ammo
-6 ability to select ammo type
-6 powerups
-6 able to pick up ammo
-6 reload system
-6 when enemy dies, spawns 2 clones (do NOT apply same rule to clones!)
-6 use spawn points
-6 enemy can run out of ammo
-6 enemy without ammo stops chasing you to look for ammo
+6 different kinds of ammo /
+6 ability to select ammo type /
+6 powerups ( change colors )
+6 able to pick up ammo /
+6 reload system x
+6 when enemy dies, spawns 2 clones (do NOT apply same rule to clones!) /
+6 use spawn points /
+6 enemy can run out of ammo /
+6 enemy without ammo stops chasing you to look for ammo /
 6 enemy low on health stops chasing you to look for health
 6 win by another method (e.g. capture the flag)
-6 UI
-6 game progresses in difficulty
-6 originality
+6 UI x
+6 game progresses in difficulty /
+6 originality x
 */
 }
