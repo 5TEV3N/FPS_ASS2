2184e4ed5f666cc4087397b9d8fd5e48749833e8
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-30 17:39:04 -0400

    NavMeshes and random hiding places implamented
    Please work on enemy bullets

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -5,14 +5,18 @@ public class EnemyController : MonoBehaviour
 {
     EnemyManager enemyManager;                   // Refference to the enemy manager
 
-    public float enemyShootingForce;
+    [Header("Values")]
+    public Vector3 playerPosition;               // Gets the information of where the player is
+    public float enemyShootingForce;             // How hard the player shoots
+
+    [Header ("Containers")]
     public GameObject enemyBullets;              // Container for the enemy bullets
+    public GameObject playerObject;              // Container for the player's charecter. so that the enemy is facing the player at all times
     public Transform bulletsParent;              // Bullet will be placed in the BulletsSpawned as a child
     public Transform enemybulletExitPoint;       // Contains the transform of where the bullet is exiting
-    public Camera enemyCam;
-
-    public GameObject playerObject;
-    public Vector3 playerPosition;
+    public Camera enemyCam;                      // The container of the player camera
+    public NavMeshAgent agent;                   // Contains the NavMeshAgent
+    public Transform[] hidingPosition;           // Hiding positions for the enemy
 
     private GameObject enemyBulletShot;          // Instantiated prefab bullet 
     
@@ -39,6 +43,8 @@ public class EnemyController : MonoBehaviour
         if (player.gameObject.tag == "Player")
         {
             ShootOnSight();
+            Transform findAnotherHidingPlace = hidingPosition[Random.Range(0, hidingPosition.Length)];
+            agent.SetDestination(findAnotherHidingPlace.position);
         }
     }
 
